fdca351a4e5a1eb68e577485d2f58f50b48375df
shreyas-londhe <shreyas_londhe@outlook.com>
2025-03-06 14:40:06 +0530

    feat: added support for external inputs

--- program/src/bin/email_verify.rs
+++ program/src/bin/email_verify.rs
@@ -1,11 +1,12 @@
 #![no_main]
 sp1_zkvm::entrypoint!(main);
 
-use zkemail_core::{abi_encode_email_verifier_output, verify_email, Email, EmailVerifierOutput};
+use zkemail_core::{verify_email, Email, EmailVerifierOutput, VerificationOutput};
 
 fn main() {
     let input = sp1_zkvm::io::read::<Email>();
     let output: EmailVerifierOutput = verify_email(&input);
-    let output = abi_encode_email_verifier_output(&output);
+    let output = VerificationOutput::from_parts(output, None);
+    let output = output.abi_encode();
     sp1_zkvm::io::commit_slice(&output);
 }
--- script/src/bin/main.rs
+++ script/src/bin/main.rs
@@ -1,12 +1,12 @@
-use alloy_sol_types::SolType;
 use clap::Parser;
 use dotenv::dotenv;
 use sp1_sdk::{include_elf, ProverClient, SP1Stdin};
 use std::path::PathBuf;
-use tracing::{error, info};
-use zkemail_core::{SolEmailVerifierOutput, SolEmailWithRegexVerifierOutput};
+use tracing::info;
+use zkemail_core::VerificationOutput;
 use zkemail_helpers::{
-    generate_email_inputs, generate_email_with_regex_inputs, read_email_file, read_regex_config,
+    generate_email_inputs, generate_email_with_regex_inputs, read_email_file, read_json_file,
+    AbiDecodable,
 };
 
 /// The ELF (executable and linkable format) file for the Succinct RISC-V zkVM.
@@ -74,14 +74,15 @@ async fn main() {
     // Generate appropriate input
     if let Some(regex_config) = args.regex_config.as_ref() {
         let raw_email = read_email_file(&args.email_path).expect("Failed to read email file");
-        let regex_info = read_regex_config(regex_config).expect("Failed to read regex config");
-        let input = generate_email_with_regex_inputs(&args.from_domain, &raw_email, &regex_info)
-            .await
-            .expect("Failed to generate email with regex inputs");
+        let regex_info = read_json_file(regex_config).expect("Failed to read regex config");
+        let input =
+            generate_email_with_regex_inputs(&args.from_domain, &raw_email, &regex_info, None)
+                .await
+                .expect("Failed to generate email with regex inputs");
         stdin.write(&input);
     } else {
         let raw_email = read_email_file(&args.email_path).expect("Failed to read email file");
-        let input = generate_email_inputs(&args.from_domain, &raw_email)
+        let input = generate_email_inputs(&args.from_domain, &raw_email, None)
             .await
             .expect("Failed to generate email inputs");
         stdin.write(&input);
@@ -92,52 +93,8 @@ async fn main() {
         let (output, report) = client.execute(image, &stdin).run().unwrap();
         info!("Program executed successfully.");
 
-        if args.regex_config.as_ref().is_some() {
-            let output = SolEmailWithRegexVerifierOutput::abi_decode(output.as_slice(), true)
-                .map_err(|err| {
-                    error!("Error decoding regex output: {:?}", err);
-                    std::process::exit(1);
-                })
-                .unwrap();
-
-            let SolEmailWithRegexVerifierOutput {
-                email:
-                    SolEmailVerifierOutput {
-                        from_domain_hash,
-                        public_key_hash,
-                        verified,
-                    },
-                header_regex_verified,
-                body_regex_verified,
-                header_regex_matches,
-                body_regex_matches,
-            } = output;
-
-            info!("From domain hash: {}", from_domain_hash);
-            info!("Public key hash: {}", public_key_hash);
-            info!("Verified: {}", verified);
-            info!("Header regex verified: {}", header_regex_verified);
-            info!("Body regex verified: {}", body_regex_verified);
-            info!("Header regex matches: {:?}", header_regex_matches);
-            info!("Body regex matches: {:?}", body_regex_matches);
-        } else {
-            let output = SolEmailVerifierOutput::abi_decode(output.as_slice(), true)
-                .map_err(|err| {
-                    error!("Error decoding basic output: {:?}", err);
-                    std::process::exit(1);
-                })
-                .unwrap();
-
-            let SolEmailVerifierOutput {
-                from_domain_hash,
-                public_key_hash,
-                verified,
-            } = output;
-
-            info!("From domain hash: {}", from_domain_hash);
-            info!("Public key hash: {}", public_key_hash);
-            info!("Verified: {}", verified);
-        };
+        let output = VerificationOutput::abi_decode(output.as_slice()).unwrap();
+        println!("Output: {:?}", output);
 
         info!("Number of cycles: {}", report.total_instruction_count());
     } else {
